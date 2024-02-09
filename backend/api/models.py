4bd5d3b3e02a01668c03d415a1fb7073231bcd40
Viridiana Ponce <viri.ponce@gmail.com>
2024-02-09 13:19:52 -0800

    Add insensitive collation to first name and last name fields

--- backend/api/models.py
+++ backend/api/models.py
@@ -8,6 +8,8 @@ from .managers import CustomUserManager
 class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(_("first name"), max_length=150, blank=True, db_collation="case_insensitive")
+    last_name = models.CharField(_("last name"), max_length=150, blank=True, db_collation="case_insensitive")
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ['first_name', 'last_name']   
