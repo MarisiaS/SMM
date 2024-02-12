26c530939e8073d9afdc2747d9b5366438dcf0fb
Viridiana Ponce <viri.ponce@gmail.com>
2024-02-12 11:26:05 -0800

    Add patch in Site view, return paginated data and validate name in serializer

--- backend/api/serializers/SiteSerializer.py
+++ backend/api/serializers/SiteSerializer.py
@@ -8,3 +8,13 @@ class SiteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Site
         fields = ('id', 'name', 'num_lanes', 'pool_len', 'len_unit')
+
+    # Validate that name is unique, if not raise a Validation Error
+    def validate_name(self, value):
+        if self.instance and self.instance.name == value:
+            return value  # No change, no validation needed for the same name
+
+        if Site.objects.filter(name=value).exists():
+            raise serializers.ValidationError('Duplicate value, it already exists.')
+
+        return value
