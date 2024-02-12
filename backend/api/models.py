d90fde60ec646e51876f794f198f413c9797d6ec
Elzbieta Sanjuan <marisiasanjuan@gmail.com>
2024-02-12 11:18:37 -0800

    rebase to main

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import MinValueValidator
+from django.db.models.functions import Coalesce
 from .managers import CustomUserManager
 
 
@@ -33,3 +34,40 @@ class School(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
     open_hour = models.TimeField(null=True, blank=True)
     close_hour = models.TimeField(null=True, blank=True)
+
+class Group(models.Model):
+    class Gender(models.TextChoices):
+        FEMALE = "F", _("Girl")
+        MALE = "M", _("Boy")
+        MIXED = "MX", _("Mixed")
+
+    gender = models.CharField(max_length=20, choices=Gender.choices, default=Gender.MIXED)
+    min_age = models.PositiveSmallIntegerField(blank=True, null=True)
+    max_age = models.PositiveSmallIntegerField(blank=True, null=True)
+
+    @property
+    def name(self):
+        gender_display = self.get_gender_display()
+
+        if gender_display != "Mixed":
+            gender_display += "s"
+
+        age_range = ""
+        if self.min_age is not None and self.max_age is not None:
+            age_range = f"{self.min_age}to{self.max_age}"
+        elif self.min_age is not None:
+            age_range = f"{self.min_age}&Above"
+        elif self.max_age is not None:
+            age_range = f"{self.max_age}&Under"
+
+        return f"{gender_display}{age_range}"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                models.ExpressionWrapper(Coalesce('min_age', models.Value(-1)), output_field=models.IntegerField()),
+                models.ExpressionWrapper(Coalesce('max_age', models.Value(-1)), output_field=models.IntegerField()),
+                models.F('gender'),
+                name='unique_group'
+            )
+        ]
