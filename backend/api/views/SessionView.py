0136dded8b8e8c8b893c4e6b58819cb39874dab6
Elzbieta Sanjuan <marisiasanjuan@gmail.com>
2024-02-13 09:39:47 -0800

    Added session view

---
+++ backend/api/views/SessionView.py
@@ -0,0 +1,16 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from api.models import Session
+from api.serializers.SessionSerializer import SessionSerializer
+from drf_spectacular.utils import extend_schema
+import logging
+
+logger = logging.getLogger('django')
+
+
+@extend_schema(tags=['Session'])
+class SessionViewSet(viewsets.ModelViewSet):
+    queryset = Session.objects.all()
+    serializer_class = SessionSerializer
+    # permission_classes = [IsAuthenticated]
+    http_method_names = ['get', 'post', 'delete', 'patch']
