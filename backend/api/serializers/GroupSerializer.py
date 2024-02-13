5858e233698af597148e1515550eae9d9eb83357
Elzbieta Sanjuan <marisiasanjuan@gmail.com>
2024-02-13 12:30:23 -0800

    Added the request changes to valitations

--- backend/api/serializers/GroupSerializer.py
+++ backend/api/serializers/GroupSerializer.py
@@ -1,13 +1,12 @@
 from api.models import Group
 from rest_framework import serializers
 from api.CustomField import BlankableIntegerField
-from django.core.validators import MinValueValidator
 
 
 class GroupSerializer(serializers.ModelSerializer):
     name = serializers.CharField(read_only=True)
-    min_age = BlankableIntegerField(required=False, validators=[MinValueValidator(5)])
-    max_age = BlankableIntegerField(required=False, validators=[MinValueValidator(6)])
+    min_age = BlankableIntegerField(required=False)
+    max_age = BlankableIntegerField(required=False)
 
     class Meta:
         model = Group
@@ -24,14 +23,23 @@ class GroupSerializer(serializers.ModelSerializer):
         """
         min_age = data.get('min_age', None)
         max_age = data.get('max_age', None)
-        if min_age and max_age and min_age >= max_age:
-            raise serializers.ValidationError("Minimum age cannot be greater or equal to maximum age")
+        if min_age is not None and min_age <= 0:
+            raise serializers.ValidationError({'min_age': "Min age must be greater than 0."})
+
+        if max_age is not None and max_age <= 0:
+            raise serializers.ValidationError({'max_age': "Max age must be greater than 0."})
+
+        if min_age is not None and max_age is not None and min_age >= max_age:
+            raise serializers.ValidationError({
+                    'min_age': "Min age must be less than or equal to max age.",
+                    'max_age': "Max age must be greater than or equal to min age."
+                })
         return data
 
     def validate_unique_group(self, data):
         # Check if the combination of min_age, max_age, and gender is unique
-        min_age = data.get('min_age', "")
-        max_age = data.get('max_age', "")
+        min_age = data.get('min_age', None)
+        max_age = data.get('max_age', None)
         gender = data.get('gender')
 
         if Group.objects.filter(
