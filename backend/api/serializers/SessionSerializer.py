4ee4dc9a77855a83a75bfe30bb77f93d0f8cfcf3
Elzbieta Sanjuan <marisiasanjuan@gmail.com>
2024-02-14 13:55:06 -0800

    Added resposnse and request examples to swagger

--- backend/api/serializers/SessionSerializer.py
+++ backend/api/serializers/SessionSerializer.py
@@ -1,23 +1,60 @@
 from api.models import Session
 from rest_framework import serializers
+from drf_spectacular.utils import extend_schema_serializer, OpenApiExample
 
 
-
+@extend_schema_serializer(
+    examples=[
+        OpenApiExample(
+            name='respose',
+            value={
+                "id": 232,
+                "name": "MWF19",
+                "days_of_week": [
+                    1,
+                    0,
+                    1,
+                    0,
+                    1,
+                    0,
+                    0
+                ],
+                "time": "19:00:00",
+                "coach": 1,
+                "school": 1
+            },
+            response_only=True,  # signal that example only applies to responses
+            ),
+        OpenApiExample(
+            name='request',
+            value={
+                "days_of_week": [
+                    1,
+                    0,
+                    1,
+                    0,
+                    1,
+                    0,
+                    0
+                ],
+                "time": "19:00:00",
+                "coach": 1,
+                "school": 1
+            },
+            request_only=True,
+            ),
+    ]
+)
 class SessionSerializer(serializers.ModelSerializer):
     name = serializers.CharField(read_only=True)
 
     class Meta:
         model = Session
         fields = ('id', 'name', 'days_of_week', 'time', 'coach', 'school')
-    
 
     def validate_days_of_week(self, value):
         if len(value) != 7:
             raise serializers.ValidationError("Days of the week has to have a lenght of 7")
-        if not all(isinstance(day, bool) for day in value):
-            raise serializers.ValidationError("All elements in days_of_week must be booleans.")
         if not any(value):
             raise serializers.ValidationError("At least a day of the week has to be chosen.")
         return value
-    
-     
