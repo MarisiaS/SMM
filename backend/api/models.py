fdcfc90a069ac8f996b57d6bc8dd4db7a888d2cb
Viridiana Ponce <viri.ponce@gmail.com>
2024-02-21 17:34:38 -0800

    Add Athlete model

--- backend/api/models.py
+++ backend/api/models.py
@@ -110,3 +110,26 @@ class Session(models.Model):
     time = models.TimeField()
     coach = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='coach_group')
     school = models.ForeignKey(School, on_delete=models.CASCADE, related_name='school_group')
+
+class EventType(models.Model):
+    class Status(models.TextChoices):
+        ACTIVE = "ACTIVE", _("Active")
+        INACTIVE = "INACTIVE", _("Inactive")
+
+
+    class Gender(models.TextChoices):
+        FEMALE = "F", _("Girl")
+        MALE = "M", _("Boy")
+    
+    first_name = models.CharField (max_length=150, blank=False, db_collation="case_insensitive")
+    last_name = models.CharField(max_length=150, blank=False, db_collation="case_insensitive")    
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=20, choices=Gender.choices)
+    status = models.CharField(max_length=20, choices=Status.choices)
+    session = models.ForeignKey(Session, on_delete=models.SET_NULL, related_name='session_group')
+    school = models.ForeignKey(School, on_delete=models.SET_NULL, related_name='school_group')
+    email = models.EmailField()
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
