6589b1ed058cb7893ce5d89dd7635335cfe46859
Elzbieta Sanjuan <marisiasanjuan@gmail.com>
2024-03-15 12:16:20 -0700

    Deleted previous serializer

--- backend/api/serializers/AthleteSeedTime.py
+++
@@ -1,9 +0,0 @@
-from api.models import Athlete
-from rest_framework import serializers
-from django.utils import timezone
-
-
-class AthleteSeedTimeSerializer(serializers.Serializer):
-    athlete = serializers.PrimaryKeyRelatedField(queryset=Athlete.objects.all())
-    athlete_full_name = serializers.CharField()
-    seed_time = serializers.CharField()
