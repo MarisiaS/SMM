159faa2c82c580071ab9adff24ecaf8acb27e46b
Elzbieta Sanjuan Szklarz <marisiasanjuan@gmail.com>
2024-03-29 18:10:14 -0700

    Created delete method to eliminated all the heat related to andevent

--- backend/api/views/GenerateHeatView.py
+++ backend/api/views/GenerateHeatView.py
@@ -1,10 +1,13 @@
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from api.models import MeetEvent, Heat, Group, Athlete
 from api.serializers.GenerateHeatSerializer import GenerateHeatSerializer
 from rest_framework.exceptions import ValidationError
 from drf_spectacular.utils import extend_schema
+from rest_framework.permissions import IsAuthenticated
+from django.db import transaction
 from django.db.models import F, Value, Func, IntegerField
 from django.utils import timezone
 
@@ -30,28 +33,39 @@ class GenerateHeatView(APIView):
         except:
             return Response({'error': 'Not able to retrieve number of lanes'}, status=status.HTTP_404_NOT_FOUND)
         
-        #Get group_id 
-        # TODO: Mover despues del is_valid()
+        #Get group_id
         try:
             group_instance = event_instance.group
         except:
             return Response({'error': 'Not able to retrieve group'}, status=status.HTTP_404_NOT_FOUND)
         
-        athletes_data = request.data.get('athletes', [])
-        queryset = self.get_queryset(group_instance.id)
-        queryset_athlete_ids = set(queryset.values_list('id', flat=True))
-        request_athlete_ids = {athlete['athlete'] for athlete in athletes_data}
-        if not request_athlete_ids.issubset(queryset_athlete_ids):
-            return Response({'error': 'One or more athletes are not part of the specified group.'}, status=status.HTTP_400_BAD_REQUEST)
-        
        
         serializer = GenerateHeatSerializer(data=request.data, context={'num_lanes': num_lanes, 'event': event_instance})
         if serializer.is_valid(raise_exception=True):
-
+            #Before saving, check that the given athletes are part of the event group
+            athletes_data = request.data.get('athletes', [])
+            queryset = self.get_queryset(group_instance.id)
+            queryset_athlete_ids = set(queryset.values_list('id', flat=True))
+            request_athlete_ids = {athlete['athlete'] for athlete in athletes_data}
+            if not request_athlete_ids.issubset(queryset_athlete_ids):
+                return Response({'error': 'One or more athletes are not part of the specified group for the event.'}, status=status.HTTP_400_BAD_REQUEST)
             serializer.save()
         return Response({'success': 'Heats generated'}, status=status.HTTP_200_OK)
     
-    #TODO: Delete method
+    @transaction.atomic
+    def delete(self, request, event_id):
+        try:
+            event_instance = MeetEvent.objects.get(id=event_id)
+        except MeetEvent.DoesNotExist:
+            return Response({'error': 'Event not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        try:
+            Heat.objects.filter(event=event_instance).delete()
+        except Exception as e:
+            transaction.set_rollback(True)
+            return Response({'error': f'Failed to delete heats: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        return Response({'success': 'Associated heats deleted'}, status=status.HTTP_200_OK)
 
     def get_queryset(self, group_id):
         try:
