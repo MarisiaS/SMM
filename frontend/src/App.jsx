eb7aba9e0763e09e0cd96e59ca63dabc35b85d45
Elzbieta Sanjuan Szklarz <marisiasanjuan@gmail.com>
2024-04-10 14:47:43 -0700

    Created a route to login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import './App.css'
+import Login from './components/login'
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <>
+      <Routes>
+        <Route path="/" element={<Login />}/>
+      </Routes>
     </>
   )
 }
