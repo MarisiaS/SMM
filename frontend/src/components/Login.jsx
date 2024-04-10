56ab61ac5f81273072178052ce71fcba8cc27ccf
Elzbieta Sanjuan Szklarz <marisiasanjuan@gmail.com>
2024-04-10 15:32:42 -0700

    Added form configuration

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,30 +3,43 @@ import { Box } from '@mui/material'
 import MyTextField from './FormElements/MyTextField'
 import MyPasswordField from './FormElements/MyPasswordField'
 import MyButton from './FormElements/MyButton'
+import {useForm} from 'react-hook-form'
 
 const Login = () =>{
+    const {handleSubmit, control} = useForm()
+
+    const submission = (data) => {
+        console.log(data)
+    }
     return(
-        <div className={"myBackground"}> 
+        <div className={"myBackground"}>
+             <form onSubmit={handleSubmit(submission)}>
                 <Box className={"whiteBox"}>
                     <Box className={"itemBox"}>
                         <Box className={"title"}> Login </Box>
                     </Box>
                     <Box className={"itemBox"}>
                         <MyTextField
-                        label={"Email"}
+                        label = {"Email"}
+                        name = {"email"}
+                        control = {control}
                         />
                     </Box>
                     <Box className={"itemBox"}>
                         <MyPasswordField
                         label={"Password"}
+                        name = {"password"}
+                        control = {control}
                         />
                     </Box>
                     <Box className={"itemBox"}>
                         <MyButton 
                             label={"Login"}
+                            type={"submit"}
                         />
                     </Box>
                 </Box>
+            </form>
         </div>
     )
 }
