3bfa99f052fb1f9583695db14d00cd1fd221f246
Elzbieta Sanjuan Szklarz <marisiasanjuan@gmail.com>
2024-05-03 11:23:38 -0700

    Token lifetime set to 60 minutes

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -161,7 +161,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
