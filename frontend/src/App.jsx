531879b1577746f5d11325ab5f35766c00121098
Viridiana Ponce <viri.ponce@gmail.com>
2024-05-23 06:13:56 -0700

    Redifine routes, adding the private routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,33 @@
-import './App.css'
-import Login from './components/Login'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import NavBar from './components/NavBar'
+import "./App.css";
+import Login from "./components/Login";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import PrivateRoutes from "./utils/privateRoutes";
 
 function App() {
+  const NotFound = () => {
+    return <h1>The requested URL was not found</h1>;
+  };
+
+  const H1 = () => {
+    return <h1>Hello from home</h1>;
+  };
+
+  const menuOptions = [
+    { id: 1, path: "/swim-meet", label: "Swim Meet", icon: "" },
+  ];
+
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />}/>
-          <Route path="/NavBar" element={<NavBar />}/>
-        </Routes>
-      </Router>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="*" element={<NotFound />} />
+      <Route element={<PrivateRoutes />}>
+        <Route element={<NavBar menuOptions={menuOptions} />}>
+          <Route path="/swim-meet" element={<H1 />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
