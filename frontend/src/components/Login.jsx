d223a4df15fc23f64c0f3aaf0edc92645f8d75ec
Elzbieta Sanjuan Szklarz <marisiasanjuan@gmail.com>
2024-06-05 12:30:40 -0700

    Changed register for rules on MyPasswordField, added default values to Login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,70 +1,75 @@
-import '../App.css'
-import { Box } from '@mui/material'
-import MyTextField from './FormElements/MyTextField'
-import MyPasswordField from './FormElements/MyPasswordField'
-import MyButton from './FormElements/MyButton'
-import {useForm} from 'react-hook-form'
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom'
-import { SmmApi } from '../SmmApi.jsx'
+import "../App.css";
+import { Box } from "@mui/material";
+import MyTextField from "./FormElements/MyTextField";
+import MyPasswordField from "./FormElements/MyPasswordField";
+import MyButton from "./FormElements/MyButton";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { SmmApi } from "../SmmApi.jsx";
 
-const Login = () =>{
-    const {handleSubmit, control, reset, register, formState: { errors, isValid}} = useForm({ mode: 'onChange' })
-    const [error, setError] = useState(null);
-    const navigate = useNavigate()
+const Login = () => {
+  const {
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onChange",
+  });
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
-    const submission = async (data) => {
-        try {
-            const response = await SmmApi.login(data);
-            sessionStorage.setItem("token", JSON.stringify(response.data));
-            navigate(`/swim-meet`)
-        } catch (error) {
-            if (error.response.status === 401) {
-                setError("Email and/or password are invalid.");
-            } else {
-                setError("Error conecting, try again!");
-            }
-            reset();
-        }
+  const submission = async (data) => {
+    try {
+      const response = await SmmApi.login(data);
+      sessionStorage.setItem("token", JSON.stringify(response.data));
+      navigate(`/swim-meet`);
+    } catch (error) {
+      if (error.response.status === 401) {
+        setError("Email and/or password are invalid.");
+      } else {
+        setError("Error conecting, try again!");
+      }
+      reset();
     }
+  };
 
-    return(
-        <div className={"myBackground"}>
-             <form onSubmit={handleSubmit(submission)}>
-                <Box className={"whiteBox"}>
-                    <Box className={"itemBox"}>
-                        <Box className={"title"}> Login </Box>
-                    </Box>
-                    <Box >
-                        {error && <span className={"errorText"}>{error}</span>}
-                    </Box>
-                    <Box className={"itemBox"}>
-                        <MyTextField
-                            label = {"Email"}
-                            name = {"email"}
-                            control = {control}
-                            rules ={{required: "Email is required"}}
-                        />
-                    </Box>
-                    <Box className={"itemBox"}>
-                        <MyPasswordField
-                            label={"Password"}
-                            name = {"password"}
-                            control = {control}
-                            {...register("password", {required: "Password is required"})}
-                        />
-                    </Box>
-                    <Box className={"itemBox"}>
-                        <MyButton 
-                            label={"Login"}
-                            type={"submit"}
-                            disabled ={!isValid}
-                        />
-                    </Box>
-                </Box>
-            </form>
-        </div>
-    )
-}
+  return (
+    <div className={"myBackground"}>
+      <form onSubmit={handleSubmit(submission)}>
+        <Box className={"whiteBox"}>
+          <Box className={"itemBox"}>
+            <Box className={"title"}> Login </Box>
+          </Box>
+          <Box>{error && <span className={"errorText"}>{error}</span>}</Box>
+          <Box className={"itemBox"}>
+            <MyTextField
+              label={"Email"}
+              name={"email"}
+              control={control}
+              rules={{ required: "Email is required" }}
+            />
+          </Box>
+          <Box className={"itemBox"}>
+            <MyPasswordField
+              label={"Password"}
+              name={"password"}
+              control={control}
+              rules={{ required: "Password is required" }}
+            />
+          </Box>
+          <Box className={"itemBox"}>
+            <MyButton label={"Login"} type={"submit"} disabled={!isValid} />
+          </Box>
+        </Box>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
