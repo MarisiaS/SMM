a7862432cf1d9fe1f4f998544da380ea035c7646
Viridiana Ponce <viri.ponce@gmail.com>
2024-08-24 10:57:11 -0700

    Display alertbox with error when unable to upload data

--- frontend/src/SwimMeet/SwimMeetDisplay.jsx
+++ frontend/src/SwimMeet/SwimMeetDisplay.jsx
@@ -9,11 +9,12 @@ import {
   Delete as DeleteIcon,
   ContentPaste as DetailsIcon,
   EmojiEvents as RankingIcon,
-  Add as AddIcon
+  Add as AddIcon,
 } from "@mui/icons-material";
 import MyButton from "../components/FormElements/MyButton";
 import { Stack, Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import AlertBox from "../components/Common/AlertBox.jsx";
 
 const columns = [
   {
@@ -39,12 +40,18 @@ const columns = [
 ];
 
 const SwimMeetDisplay = () => {
+  const [errorOnLoading, setErrorOnLoading] = useState(false);
   const [data, setData] = useState([]);
   const [searchPar, setSearchPar] = useState("");
   const navigate = useNavigate();
   const [offset, setOffset] = useState(0);
   const [count, setCount] = useState(0);
   const [limit, setLimit] = useState(10);
+
+  let typeAlertLoading = errorOnLoading ? "error" : "success";
+  let messageOnLoading = errorOnLoading
+    ? "Data upload failed. Please try again!"
+    : "";
 
   const handleDetailsClick = (id) => {
     console.log("Details ...", id);
@@ -63,26 +70,50 @@ const SwimMeetDisplay = () => {
   };
 
   const actions = [
-    { name: "Details", icon: <DetailsIcon />, onClick: handleDetailsClick, tip:"Go to events" },
-    { name: "Edit", icon: <EditIcon />, onClick: handleEditClick, tip:"Edit basic information" },
-    { name: "Delete", icon: <DeleteIcon />, onClick: handleDeleteClick, tip: "Delete" },
-    { name: "Ranking", icon: <RankingIcon />, onClick: handleRankingClick, tip: "Go to ranking" },
+    {
+      name: "Details",
+      icon: <DetailsIcon />,
+      onClick: handleDetailsClick,
+      tip: "Go to events",
+    },
+    {
+      name: "Edit",
+      icon: <EditIcon />,
+      onClick: handleEditClick,
+      tip: "Edit basic information",
+    },
+    {
+      name: "Delete",
+      icon: <DeleteIcon />,
+      onClick: handleDeleteClick,
+      tip: "Delete",
+    },
+    {
+      name: "Ranking",
+      icon: <RankingIcon />,
+      onClick: handleRankingClick,
+      tip: "Go to ranking",
+    },
   ];
 
   useEffect(() => {
     let ignore = false;
     async function fetching() {
-      const json = await SmmApi.getSwimMeetList(searchPar, offset, limit);
-      console.log(json);
-      if (!ignore) {
-        const formattedData = json.results.map(item => ({
-          ...item,
-          date: dayjs(item.date).format('MM/DD/YYYY'),
-          time: item.time.slice(0, 5),
-        }));
+      try {
+        const json = await SmmApi.getSwimMeetList(searchPar, offset, limit);
+        console.log(json);
+        if (!ignore) {
+          const formattedData = json.results.map((item) => ({
+            ...item,
+            date: dayjs(item.date).format("MM/DD/YYYY"),
+            time: item.time.slice(0, 5),
+          }));
 
-        setData(formattedData);
-        setCount(json.count)
+          setData(formattedData);
+          setCount(json.count);
+        }
+      } catch (error) {
+        setErrorOnLoading(true);
       }
     }
     fetching();
@@ -95,22 +126,46 @@ const SwimMeetDisplay = () => {
     navigate("/add-swim-meet");
   };
 
-  return (
-    <div>
-      <Stack direction="row" alignItems="center" justifyContent="space-between">
-        <Box sx={{ marginLeft: 5 }}>
-          <MyButton label={"Swim Meet"} onClick={handleAddNew}>
-            <AddIcon/>
-          </MyButton>
-        </Box>
-        <Box className={"searchBox"} sx={{ marginRight: 5 }}>
-          <SearchBar setSearchPar={setSearchPar}></SearchBar>
-        </Box>
+  if (errorOnLoading) {
+    return (
+      <Stack
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "16px",
+          width: "300px",
+          margin: "auto",
+        }}
+      >
+        <AlertBox type={typeAlertLoading} message={messageOnLoading} />
       </Stack>
-      <GenericTable data={data} columns={columns} actions={actions} />
-      <PaginationBar count={count} setOffset={setOffset} setLimit={setLimit}></PaginationBar>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Box sx={{ marginLeft: 5 }}>
+            <MyButton label={"Swim Meet"} onClick={handleAddNew}>
+              <AddIcon />
+            </MyButton>
+          </Box>
+          <Box className={"searchBox"} sx={{ marginRight: 5 }}>
+            <SearchBar setSearchPar={setSearchPar}></SearchBar>
+          </Box>
+        </Stack>
+        <GenericTable data={data} columns={columns} actions={actions} />
+        <PaginationBar
+          count={count}
+          setOffset={setOffset}
+          setLimit={setLimit}
+        ></PaginationBar>
+      </div>
+    );
+  }
 };
 
 export default SwimMeetDisplay;
