f989ed549e68be1ad315f3aeff581e5d2d025177
Elzbieta María Sanjuan Szklarz <95152310+MarisiaS@users.noreply.github.com>
2024-09-18 10:56:32 -0700

    Eliminate import to Container on AddEventForm.jsx

--- frontend/src/Event/AddEventForm.jsx
+++ frontend/src/Event/AddEventForm.jsx
@@ -1,5 +1,5 @@
 import "../App.css";
-import { Box, Stack, Container } from "@mui/material";
+import { Box, Stack} from "@mui/material";
 import MyButton from "../components/FormElements/MyButton.jsx";
 import MySelect from "../components/FormElements/MySelect.jsx";
 
