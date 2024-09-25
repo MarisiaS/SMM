0bc9336965623e3add93d97a34479014e98118f7
Elzbieta Sanjuan Szklarz <marisiasanjuan@gmail.com>
2024-09-25 09:13:54 -0700

    Add extra action button always visible, reduce data size.

--- frontend/src/Tests/TestConditionalRender.jsx
+++ frontend/src/Tests/TestConditionalRender.jsx
@@ -1,165 +1,64 @@
 import ExpandableTable from "../components/Common/ExpandableTable";
 import GenericTable from "../components/Common/GenericTable";
-import {
-    Edit as EditIcon,
-} from "@mui/icons-material";
+import { Typography } from "@mui/material";
+import { Edit as EditIcon, Delete as DeleteIcon } from "@mui/icons-material";
 
 const testData = [
-    {
-      id: 1,
-      name: "Heat 1",
-      heats: [
-        {
-          id: 12,
-          lane: 1,
-          athlete_full_name: "Hola",
-          seed_time: 2,
-          heat_time: "",
-        },
-        {
-          id: 13,
-          lane: 2,
-          athlete_full_name: "Adios",
-          seed_time: 2,
-          heat_time: "",
-        },
-        {
-          id: 22,
-          lane: 3,
-          athlete_full_name: "Carlos",
-          seed_time: 2,
-          heat_time: "",
-        },
-        {
-          id: 23,
-          lane: 4,
-          athlete_full_name: "Gerardo",
-          seed_time: 2,
-          heat_time: "",
-        },
-        {
-          id: 24,
-          lane: 5,
-          athlete_full_name: "Pedro",
-          seed_time: 2,
-          heat_time: "",
-        },
-      ],
-    },
-    {
-      id: 2,
-      name: "Heat 2",
-      heats: [
-        {
-          id: 14,
-          lane: 1,
-          athlete_full_name: "Hola",
-          seed_time: 2,
-          heat_time: "",
-        },
-      ],
-    },
-    {
-      id: 3,
-      name: "Heat 3",
-      heats: [
-        {
-          id: 22,
-          lane: 1,
-          athlete_full_name: "Hola",
-          seed_time: 2,
-          heat_time: "",
-        },
-        {
-          id: 23,
-          lane: 2,
-          athlete_full_name: "Adios",
-          seed_time: 2,
-          heat_time: "",
-        },
-      ],
-    },
-    {
-      id: 4,
-      name: "Heat 4",
-      heats: [
-        {
-          id: 22,
-          lane: 1,
-          athlete_full_name: "Hola",
-          seed_time: 2,
-          heat_time: "",
-        },
-        {
-          id: 23,
-          lane: 2,
-          athlete_full_name: "Adios",
-          seed_time: 2,
-          heat_time: "",
-        },
-      ],
-    },
-    {
-      id: 5,
-      name: "Heat 5",
-      heats: [
-        {
-          id: 22,
-          lane: 1,
-          athlete_full_name: "Hola",
-          seed_time: 2,
-          heat_time: "",
-        },
-        {
-          id: 23,
-          lane: 2,
-          athlete_full_name: "Adios",
-          seed_time: 2,
-          heat_time: "",
-        },
-      ],
-    },
-    {
-      id: 6,
-      name: "Heat 6",
-      heats: [
-        {
-          id: 22,
-          lane: 1,
-          athlete_full_name: "Hola",
-          seed_time: 2,
-          heat_time: "",
-        },
-        {
-          id: 23,
-          lane: 2,
-          athlete_full_name: "Adios",
-          seed_time: 2,
-          heat_time: "",
-        },
-      ],
-    },
-    {
-      id: 7,
-      name: "Heat 7",
-      heats: [
-        {
-          id: 22,
-          lane: 1,
-          athlete_full_name: "Hola",
-          seed_time: 2,
-          heat_time: "",
-        },
-        {
-          id: 23,
-          lane: 2,
-          athlete_full_name: "Adios",
-          seed_time: 2,
-          heat_time: "",
-        },
-      ],
-    },
-  ];
+  {
+    id: 1,
+    name: "Heat 1",
+    heats: [
+      {
+        id: 12,
+        lane: 1,
+        athlete_full_name: "Hola",
+        seed_time: 2,
+        heat_time: "",
+      },
+      {
+        id: 13,
+        lane: 2,
+        athlete_full_name: "Adios",
+        seed_time: 2,
+        heat_time: "",
+      },
+      {
+        id: 22,
+        lane: 3,
+        athlete_full_name: "Carlos",
+        seed_time: 2,
+        heat_time: "",
+      },
+      {
+        id: 23,
+        lane: 4,
+        athlete_full_name: "Gerardo",
+        seed_time: 2,
+        heat_time: "",
+      },
+      {
+        id: 24,
+        lane: 5,
+        athlete_full_name: "Pedro",
+        seed_time: 2,
+        heat_time: "",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Heat 2",
+    heats: [
+      {
+        id: 14,
+        lane: 1,
+        athlete_full_name: "Hola",
+        seed_time: 2,
+        heat_time: "",
+      },
+    ],
+  },
+];
 
 const mainTableColumns = [
   {
@@ -193,29 +92,54 @@ const subTableColumns = [
 ];
 
 const TestConditionalRender = () => {
+  const handleEditClick = () => {
+    console.log("Edit ...");
+  };
 
-    const handleEditClick = () => {
-        console.log("Edit ...");
-      };
+  const handleDeleteClick = () => {
+    console.log("Delete ...");
+  };
 
-    const actions = [
-        {
-          name: "Edit",
-          icon: <EditIcon />,
-          onClick: handleEditClick,
-          tip: "Edit athletes",
-          visible: (row) => row.original.name == "Heat 1",
-        },
-      ];
+  const actions = [
+    {
+      name: "Edit",
+      icon: <EditIcon />,
+      onClick: handleEditClick,
+      tip: "Edit athletes",
+      visible: (row) => row.original.name == "Heat 1",
+    },
+    {
+      name: "Delete",
+      icon: <DeleteIcon />,
+      onClick: handleDeleteClick,
+      tip: "Delete",
+    },
+  ];
   return (
     <div>
+      <Typography
+        key={1}
+        variant={"h4"}
+        component="div"
+        sx={{ color: "text.secondary" }}
+      >
+        Expandable table
+      </Typography>
       <ExpandableTable
         data={testData}
         columns={mainTableColumns}
         actions={actions}
         subTableColumns={subTableColumns}
-        subData={'heats'}
+        subData={"heats"}
       />
+      <Typography
+        key={2}
+        variant={"h4"}
+        component="div"
+        sx={{ color: "text.secondary" }}
+      >
+        Generic table
+      </Typography>
       <GenericTable
         data={testData}
         columns={mainTableColumns}
