982f354dff65915b190da673c64d286a8af10dab
Elzbieta Sanjuan Szklarz <marisiasanjuan@gmail.com>
2024-09-25 09:11:03 -0700

    Add path to TestConditionalRender

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import AddEvent from "./Event/AddEvent";
 import TestTabPanel from "./Tests/TestTabPanel";
 import TestItemPaginationBar from "./Tests/TestItemPaginationBar";
 import TestExpandableTable from "./Tests/TestExpandableTable";
+import TestConditionalRender from "./Tests/TestConditionalRender";
 
 function App() {
   const NotFound = () => {
@@ -43,6 +44,7 @@ function App() {
       <Route path="/tests/test-tab-panel" element={<TestTabPanel />} />
       <Route path="/tests/test-item-pagination-bar" element={<TestItemPaginationBar />} />
       <Route path="/tests/test-expandable-table" element={<TestExpandableTable />} />
+      <Route path="/tests/test-conditional-render" element={<TestConditionalRender />} />
     </Routes>
   );
 }
