dd7b1a373723a8df2f44cece434bc0e9214c7173
Elzbieta Sanjuan Szklarz <marisiasanjuan@gmail.com>
2024-10-21 17:15:54 -0700

    Eliminate formatSeedTime, now it is imported from helperFunctions.

--- frontend/src/Event/EventDetails.jsx
+++ frontend/src/Event/EventDetails.jsx
@@ -7,21 +7,7 @@ import { ContentPaste as BackIcon } from "@mui/icons-material";
 import AlertBox from "../components/Common/AlertBox.jsx";
 import { Build as BuildIcon } from "@mui/icons-material";
 import { Stack } from "@mui/material";
-
-const formatSeedTime = (seedTime) => {
-  if (!seedTime) return null;
-  if (seedTime === "NT") return "NT";
-  const timeParts = seedTime.split(":");
-  const secondsAndMillis = parseFloat(timeParts[2]).toFixed(2);
-  if (timeParts[0] !== "00") {
-    return `${timeParts[0]}:${timeParts[1]}:${secondsAndMillis}`;
-  }
-  if (timeParts[1] !== "00") {
-    const minutes = parseInt(timeParts[1], 10);
-    return `${minutes}:${secondsAndMillis}`;
-  }
-  return `${secondsAndMillis}`;
-};
+import { formatSeedTime } from "../utils/helperFunctions.js"; 
 
 const EventDetails = ({
   eventName,
