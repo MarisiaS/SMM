1a10ebe778c732bb9baa166ec1511d51f095e718
Elzbieta Sanjuan Szklarz <marisiasanjuan@gmail.com>
2024-11-13 00:48:36 -0800

    Post method returns the created object.

--- backend/api/views/MeetEventView.py
+++ backend/api/views/MeetEventView.py
@@ -69,8 +69,9 @@ class MeetEventView(APIView):
         }
         serializer = MeetEventSerializer(data=event_data)
         if serializer.is_valid():
-            serializer.save()
-            return Response({'success': 'Event added to the swim meet'}, status=status.HTTP_200_OK)
+            created_event = serializer.save()
+            event_serializer = MeetEventSerializer(created_event)
+            return Response(event_serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
