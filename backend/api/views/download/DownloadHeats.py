7e7a6e9e9484046a0e590cb591ae73ccc67dc9d9
Viridiana Ponce <viri.ponce@gmail.com>
2024-11-17 11:50:16 -0800

    Refactor code to be reusable and change file format

--- backend/api/views/download/DownloadHeats.py
+++ backend/api/views/download/DownloadHeats.py
@@ -9,136 +9,179 @@ from rest_framework.views import APIView
 from datetime import timedelta
 
 
-@extend_schema(tags=['Download'])
-class DownloadHeats(APIView):
+def create_excel_file_for_heats_details(swim_meet_details, event_data):
 
-    def generate_excel_for_event_heats(self, swim_meet_name, event_name, heats_data, lanes_data):
-        # Create an in-memory workbook
-        workbook = Workbook()
-        heat_worksheet = workbook.active
-        heat_worksheet.title = "By Heats"
+    workbook = Workbook()
+    # ========================= By Heats Worksheet =========================
+    heats_worksheet = workbook.active
+    heats_worksheet.title = "By Heats"
+    heats_headers = ["Lane", "Athlete", "Seed Time", "Heat Time"]
+    current_row = 1
 
-        # Headers for the heats table
-        heats_headers = ["Lane", "Athlete", "Seed Time", "Heat Time"]
+    heats_worksheet.merge_cells(
+        start_row=1, start_column=1, end_row=1, end_column=len(heats_headers))
+    swim_meet_cell = heats_worksheet.cell(row=1, column=1)
+    swim_meet_cell.value = f"{swim_meet_details['name']}\n{swim_meet_details['date']}\n{swim_meet_details['location']}"
+    swim_meet_cell.style = 'Title'
+    swim_meet_cell.alignment = Alignment(
+        horizontal="center", vertical="center", wrap_text=True)
 
-        current_row = 1
-
-        title_cell = heat_worksheet.cell(row=current_row, column=1)
-        title_cell.value = event_name
-        heat_worksheet.merge_cells(
+    current_row += 3
+    for event in event_data:
+        event_cell = heats_worksheet.cell(row=current_row, column=1)
+        event_cell.value = event["event_name"]
+        heats_worksheet.merge_cells(
             start_row=current_row, start_column=1, end_row=current_row, end_column=len(heats_headers))
-        title_cell = heat_worksheet.cell(row=current_row, column=1)
-        title_cell.style = 'Title'
-        title_cell.alignment = Alignment(
-            horizontal='center', vertical='center')
-
+        event_cell.style = 'Headline 1'
         current_row += 2
 
-        for heat in heats_data:
-            heat_name = heat.get("heat_name")
-            lanes = heat.get("lanes", [])
+        if event["heats"]:
+            for heat in event["heats"]:
+                heat_name = heat["heat_name"]
+                lanes = heat["lanes"]
+                heat_cell = heats_worksheet.cell(row=current_row, column=1)
+                heat_cell.value = heat_name
+                heats_worksheet.merge_cells(
+                    start_row=current_row, start_column=1, end_row=current_row, end_column=len(heats_headers))
+                heat_cell.style = 'Headline 2'
 
-            # Set the title for each heat in the first cell of the row
-            title_cell = heat_worksheet.cell(row=current_row, column=1)
-            title_cell.value = heat_name
-            heat_worksheet.merge_cells(
-                start_row=current_row, start_column=1, end_row=current_row, end_column=len(heats_headers))
-            title_cell.style = 'Title'
-
-            current_row += 1
-
-            # Set Headers
-            for col_num, header in enumerate(heats_headers, start=1):
-                cell = heat_worksheet.cell(row=current_row, column=col_num)
-                cell.value = header
-                cell.style = 'Headline 1'
-
-            # Populate the table with lane data for the current heat
-            for lane in lanes:
                 current_row += 1
-                heat_worksheet.cell(row=current_row, column=1,
-                                    value=lane.get("lane_num"))
-                heat_worksheet.cell(row=current_row, column=2,
-                                    value=lane.get("athlete_full_name"))
-                heat_worksheet.cell(row=current_row, column=3,
-                                    value=lane.get("seed_time"))
-                heat_worksheet.cell(row=current_row, column=4,
-                                    value=lane.get("heat_time"))
 
+                # Headers
+                for col_num, header in enumerate(heats_headers, start=1):
+                    header_cell = heats_worksheet.cell(
+                        row=current_row, column=col_num)
+                    header_cell.value = header
+                    header_cell.style = 'Headline 3'
+
+                current_row += 1
+
+                # Data
+                for lane in lanes:
+                    heats_worksheet.cell(
+                        row=current_row, column=1, value=lane["lane_num"])
+                    heats_worksheet.cell(
+                        row=current_row, column=2, value=lane["athlete_full_name"])
+                    heats_worksheet.cell(
+                        row=current_row, column=3, value=lane["seed_time"])
+                    heats_worksheet.cell(
+                        row=current_row, column=4, value=lane["heat_time"])
+                    current_row += 1
+                current_row += 2
+        else:
+
+            heats_worksheet.cell(row=current_row, column=1,
+                                 value="This event has no heats yet.")
             current_row += 2
 
-        # Adjust column widths
-        for col_num, header in enumerate(heats_headers, 1):
-            col_letter = get_column_letter(col_num)
-            heat_worksheet.column_dimensions[col_letter].width = 15
+    for col_num in range(1, len(heats_headers) + 1):
+        col_letter = get_column_letter(col_num)
+        heats_worksheet.column_dimensions[col_letter].width = 15
 
-       # ========================= Lanes Data Worksheet =========================
-        lanes_worksheet = workbook.create_sheet(title="By Lanes")
+    # ========================= By Lanes Worksheet =========================
+    lanes_worksheet = workbook.create_sheet(title="By Lanes")
+    lanes_headers = ["Heat ", "Heat Time"]
+    current_row = 1
 
-        # Headers for the lanes data
-        lanes_headers = ["Heat", "Athlete", "Heat Time"]
-        current_row = 1
+    lanes_worksheet.merge_cells(
+        start_row=1, start_column=1, end_row=1, end_column=len(heats_headers))
+    swim_meet_cell = lanes_worksheet.cell(row=1, column=1)
+    swim_meet_cell.value = f"{swim_meet_details['name']}\n{swim_meet_details['date']}\n{swim_meet_details['location']}"
+    swim_meet_cell.style = 'Title'
+    swim_meet_cell.alignment = Alignment(
+        horizontal="center", vertical="center", wrap_text=True)
 
-        title_cell = lanes_worksheet.cell(row=current_row, column=1)
-        title_cell.value = event_name
+    current_row += 3
+
+    for event in event_data:
+        event_cell = lanes_worksheet.cell(row=current_row, column=1)
+        event_cell.value = event["event_name"]
         lanes_worksheet.merge_cells(
             start_row=current_row, start_column=1, end_row=current_row, end_column=len(lanes_headers))
-        title_cell = lanes_worksheet.cell(row=current_row, column=1)
-        title_cell.style = 'Title'
-        title_cell.alignment = Alignment(
+        event_cell.alignment = Alignment(
             horizontal='center', vertical='center')
+        event_cell.style = 'Headline 1'
 
         current_row += 2
 
-        for lane in lanes_data:
-            lane_name = lane.get("lane_name")
-            heats = lane.get("heats", [])
+        if event["heats"]:
+            for lane in event["lanes"]:
+                lane_name = lane["lane_name"]
+                heats = lane["heats"]
 
-            # Set the title for each heat in the first cell of the row
-            title_cell = lanes_worksheet.cell(row=current_row, column=1)
-            title_cell.value = lane_name
-            lanes_worksheet.merge_cells(
-                start_row=current_row, start_column=1, end_row=current_row, end_column=len(lanes_headers))
-            title_cell.style = 'Title'
-
-            current_row += 1
-
-            # Set headers
-            for col_num, header in enumerate(lanes_headers, start=1):
-                cell = lanes_worksheet.cell(row=current_row, column=col_num)
-                cell.value = header
-                cell.style = 'Headline 1'
-
-            # Populate the table with heat data for the current lane
-            for heat in heats:
+                lane_cell = lanes_worksheet.cell(row=current_row, column=1)
+                lane_cell.value = lane_name
+                lanes_worksheet.merge_cells(
+                    start_row=current_row, start_column=1, end_row=current_row, end_column=len(lanes_headers))
                 current_row += 1
-                lanes_worksheet.cell(row=current_row, column=1,
-                                     value=heat.get("num_heat"))
-                lanes_worksheet.cell(row=current_row, column=2,
-                                     value=heat.get("athlete_full_name"))
-                lanes_worksheet.cell(row=current_row, column=3,
-                                     value=heat.get("heat_time"))
+                lane_cell.style = 'Headline 2'
 
+                # Headers
+                header_cell = lanes_worksheet.cell(
+                    row=current_row, column=2)
+                header_cell.value = lanes_headers[1]
+                header_cell.style = 'Headline 3'
+
+                # Data
+                for heat in heats:
+                    header_cell = lanes_worksheet.cell(
+                        row=current_row, column=1)
+                    header_cell.value = lanes_headers[0] + \
+                        str(heat["num_heat"])
+                    header_cell.style = 'Headline 3'
+                    current_row += 1
+
+                    lanes_worksheet.cell(
+                        row=current_row, column=1, value=heat["athlete_full_name"])
+                    lanes_worksheet.cell(
+                        row=current_row, column=2, value=heat["heat_time"])
+                    current_row += 2
+
+                current_row += 1
+
+        else:
+
+            lanes_worksheet.cell(row=current_row, column=1,
+                                 value="This event has no heats yet.")
             current_row += 2
 
-        # Adjust column widths for readability in both worksheets
-        for col_num in range(1, len(lanes_headers) + 1):
-            col_letter = get_column_letter(col_num)
-            lanes_worksheet.column_dimensions[col_letter].width = 15
+    for col_num in range(1, len(lanes_headers) + 1):
+        col_letter = get_column_letter(col_num)
+        lanes_worksheet.column_dimensions[col_letter].width = 15
 
-        # Save the workbook to an in-memory file
-        file_buffer = BytesIO()
-        workbook.save(file_buffer)
-        file_buffer.seek(0)
+    # Save workbook to an in-memory file
+    file_buffer = BytesIO()
+    workbook.save(file_buffer)
+    file_buffer.seek(0)
 
-        # Create an HTTP response with the Excel file for download
-        response = HttpResponse(
-            file_buffer,
-            content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        )
-        filename = swim_meet_name + "-" + event_name + ".xlsx"
-        response["Content-Disposition"] = 'attachment; filename=' + filename
-        return response
+    return file_buffer
+
+
+def format_time(time):
+    if not time:
+        return None
+    if time == timedelta(days=200):
+        return "NT"
+    elif time == timedelta(days=300):
+        return "NS"
+    elif time == timedelta(days=400):
+        return "DQ"
+
+    time_parts = str(time).split(":")
+    seconds_and_millis = f"{float(time_parts[2]):.2f}"
+
+    if time_parts[0] != "0":
+        return f"{time_parts[0]}:{time_parts[1]}:{seconds_and_millis}"
+
+    if time_parts[1] != "00":
+        minutes = int(time_parts[1])
+        return f"{minutes}:{seconds_and_millis}"
+
+    return seconds_and_millis
+
+
+@extend_schema(tags=['Download'])
+class DownloadAllHeatsByEvent(APIView):
 
     @extend_schema(
         summary='Send Binary response',
@@ -148,60 +191,80 @@ class DownloadHeats(APIView):
         }
     )
     def post(self, request, event_id):
+
+        event_data = []
+
         # Get event instance
         event_instance = MeetEvent.objects.get(id=event_id)
         event_name = event_instance.name
 
         # Get number of lanes on the event
         swim_meet_instance = event_instance.swim_meet
-        num_lanes = swim_meet_instance.site.num_lanes
 
-        swim_meet_name = swim_meet_instance.name
+        swim_meet_details = {
+            'name': swim_meet_instance.name,
+            'date': swim_meet_instance.date.strftime('%m-%d-%Y'),
+            'location': swim_meet_instance.site.name
+        }
+
+        num_lanes = swim_meet_instance.site.num_lanes
 
         max_num_heat = event_instance.total_num_heats
 
-        if max_num_heat:
-            by_heats_data = []
-            for heat_num in range(1, max_num_heat+1):
-                lanes_data = []
-                for lane_num in range(1, num_lanes + 1):
-                    lane = Heat.objects.filter(
-                        event_id=event_id, num_heat=heat_num, lane_num=lane_num).first()
-                    seed_time = lane.seed_time if lane else None
-                    if seed_time and seed_time == timedelta(days=200):
-                        seed_time = "NT"
-                    lanes_data.append(
-                        {
-                            "lane_num": lane_num,
-                            "athlete_full_name": lane.athlete.full_name if lane and lane.athlete else None,
-                            "seed_time": seed_time,
-                            "heat_time": lane.heat_time if lane else None,
-                        }
-                    )
+        by_heats_data = []
+        for heat_num in range(1, max_num_heat+1):
+            lanes_data = []
+            for lane_num in range(1, num_lanes + 1):
+                lane = Heat.objects.filter(
+                    event_id=event_id, num_heat=heat_num, lane_num=lane_num).first()
+                seed_time = format_time(lane.seed_time) if lane else ""
+                heat_time = format_time(lane.heat_time) if lane else ""
+                lanes_data.append(
+                    {
+                        "lane_num": lane_num,
+                        "athlete_full_name": lane.athlete.full_name if lane and lane.athlete else None,
+                        "seed_time": seed_time,
+                        "heat_time": heat_time,
+                    }
+                )
 
-                by_heats_data.append({
-                    "id": heat_num,
-                    "heat_name": f"Heat {heat_num}",
-                    "lanes": lanes_data
-                })
-            by_lanes_data = []
+            by_heats_data.append({
+                "id": heat_num,
+                "heat_name": f"Heat {heat_num} of {max_num_heat}",
+                "lanes": lanes_data
+            })
+        by_lanes_data = []
 
-            for lane_num in range(1, num_lanes+1):
-                heats_data = []
-                for heat_num in range(1, max_num_heat + 1):
-                    heat = Heat.objects.filter(
-                        event_id=event_id, lane_num=lane_num, num_heat=heat_num).first()
-                    heats_data.append(
-                        {
-                            "num_heat": heat_num,
-                            "athlete_full_name": heat.athlete.full_name if heat and heat.athlete else None,
-                            "heat_time": heat.heat_time if heat else None,
-                        }
-                    )
-                by_lanes_data.append({
-                    "id": lane_num,
-                    "lane_name": f"Lane {lane_num}",
-                    "heats": heats_data
-                })
+        for lane_num in range(1, num_lanes+1):
+            heats_data = []
+            for heat_num in range(1, max_num_heat + 1):
+                heat = Heat.objects.filter(
+                    event_id=event_id, lane_num=lane_num, num_heat=heat_num).first()
+                heat_time = format_time(heat.heat_time) if heat else ""
+                heats_data.append(
+                    {
+                        "num_heat": heat_num,
+                        "athlete_full_name": heat.athlete.full_name if heat and heat.athlete else None,
+                        "heat_time": heat_time,
+                    }
+                )
 
-        return self.generate_excel_for_event_heats(swim_meet_name, event_name, by_heats_data, by_lanes_data)
+            by_lanes_data.append({
+                "id": lane_num,
+                "lane_name": f"Lane {lane_num} of {num_lanes}",
+                "heats": heats_data
+            })
+        event_data.append(
+            {"event_name": event_name, "heats": by_heats_data, "lanes": by_lanes_data})
+
+        file = create_excel_file_for_heats_details(
+            swim_meet_details, event_data)
+        file_name = swim_meet_instance.name + "-" + event_name + ".xlsx"
+
+        # Create HTTP response
+        response = HttpResponse(
+            file,
+            content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
+        response["Content-Disposition"] = 'attachment; filename=' + file_name
+        return response
