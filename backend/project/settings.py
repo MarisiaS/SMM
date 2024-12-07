8356b70c091cbcfcce5e819912366ab3c507df3b
Elzbieta Sanjuan Szklarz <marisiasanjuan@gmail.com>
2024-12-07 12:02:10 -0800

    Eliminated PAGE_SIZE from settings.

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -148,7 +148,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
 }
 
 SPECTACULAR_SETTINGS = {
