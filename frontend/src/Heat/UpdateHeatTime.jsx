831cc3d0aa40c7a2c4fbd96c60c984223530eda0
Viridiana Ponce Angulo <102187795+viriponce@users.noreply.github.com>
2024-12-07 17:35:34 -0800

    FE- Edit Single Heat Time (#191)
    * Form to update Individual heat time
    
    * Component to handle updating a heat time
    
    * Add edit single heat time functionality
    
    * Remove no needed code
    
    * Move onLaneDataUpdate inside Timeout in order to allow the alertbox to be display before re-rendering
    
    * Format tips with uppercase
    
    * Added NS and DQ as options on UpdateHEatTimeForm, fixed css to consider this change.
    
    ---------
    
    Co-authored-by: Elzbieta Sanjuan Szklarz <marisiasanjuan@gmail.com>
    Co-authored-by: Elzbieta María Sanjuan Szklarz <95152310+MarisiaS@users.noreply.github.com>

---
+++ frontend/src/Heat/UpdateHeatTime.jsx
@@ -0,0 +1,84 @@
+import { Stack } from "@mui/material";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import "../App.css";
+import AlertBox from "../components/Common/AlertBox.jsx";
+import { SmmApi } from "../SmmApi.jsx";
+import { formatSeedTime } from "../utils/helperFunctions.js";
+import UpdateHeatTimeForm from "./UpdateHeatTimeForm.jsx";
+
+const UpdateHeatTime = ({ heat, onUpdate, onCancel }) => {
+  const [error, setError] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+
+  const {
+    handleSubmit,
+    control,
+    formState: { isValid },
+  } = useForm({
+    defaultValues: {
+      heat_time: "",
+    },
+    mode: "onChange",
+  });
+
+  // AlertBox variables
+  let typeAlert = error ? "error" : "success";
+  let message = error ? error : "Heat time updated successfully!";
+
+  const handleClearError = () => {
+    setError(false);
+    setSubmitted(false);
+  };
+
+  const onSubmit = async (data) => {
+    setError(false);
+
+    const payload = [
+      {
+        heat_id: Number(heat.id),
+        heat_time:
+          data.heat_time === "DQ" || data.heat_time === "NS"
+            ? data.heat_time
+            : `00:${data.heat_time}`,
+      },
+    ];
+
+    try {
+      await SmmApi.registerHeatTimes(payload);
+      onUpdate();
+    } catch (error) {
+      setError("Failed to update heat time. Please try again.");
+    }
+    setSubmitted(true);
+  };
+
+  return (
+    <div
+      onClick={handleClearError}
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "75vh", 
+        overflow: "hidden", 
+      }}
+    >
+      <Stack alignItems="center" justifyContent="space-between">
+        {submitted && <AlertBox type={typeAlert} message={message} />}
+        <UpdateHeatTimeForm
+          handleSubmit={handleSubmit(onSubmit)}
+          control={control}
+          handleCancel={onCancel}
+          data={{
+            athlete_name: heat.athlete_full_name,
+            heat_time: formatSeedTime(heat.heat_time),
+          }}
+          isValid={isValid}
+        />
+      </Stack>
+    </div>
+  );
+};
+
+export default UpdateHeatTime;
---
+++ frontend/src/Heat/UpdateHeatTimeForm.jsx
@@ -0,0 +1,68 @@
+import "../App.css";
+import { Box, Stack, Typography, Divider } from "@mui/material";
+import TimeEntryField from "../components/FormElements/TimeEntryField.jsx";
+import MyButton from "../components/FormElements/MyButton.jsx";
+
+const UpdateHeatTimeForm = ({
+  handleSubmit,
+  control,
+  handleCancel,
+  data,
+  isValid,
+}) => {
+  return (
+    <form onSubmit={handleSubmit} className={"heatTimeFormBox"}>
+      <Stack>
+        <Box>
+          <Typography
+            variant="subtitle1"
+            color="primary"
+            padding={0.5}
+            align="center"
+            style={{ fontWeight: 600 }}
+          >
+            UPDATE HEAT TIME
+          </Typography>
+        </Box>
+        <Divider sx={{ borderBottomWidth: 3 }}></Divider>
+        <Box>
+          <Typography variant="h6" padding={1} align="center" color="primary">
+            {data.athlete_name}
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant="subtitle1" padding={0.5} color="primary">
+            Current Heat Time: {data.heat_time}
+          </Typography>
+        </Box>
+        <Box className={"itemBox"}>
+          <TimeEntryField
+            label={"New Heat Time"}
+            name={"heat_time"}
+            control={control}
+            rules={{
+              required: "Heat time is required",
+              pattern: {
+                value: /^([0-5]?[0-9]):[0-5][0-9]\.[0-9]{1,2}$|^NS$|^DQ$/,
+                message: "Invalid time format",
+              },
+            }}
+            aditional_options={["NS", "DQ"]}
+          />
+        </Box>
+      </Stack>
+      <Stack className={"itemBox"}>
+        <MyButton
+          key={"create"}
+          label={"Update"}
+          type={"submit"}
+          disabled={!isValid}
+        />
+        <Box sx={{ marginTop: 2 }}></Box>
+        <MyButton key={"cancel"} label={"Cancel"} onClick={handleCancel} />
+      </Stack>
+    </form>
+  );
+};
+
+export default UpdateHeatTimeForm;
