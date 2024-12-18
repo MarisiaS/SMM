555414c9a81ad12eeb3a9cc1b9bac9e4cf873274
Elzbieta Sanjuan Szklarz <marisiasanjuan@gmail.com>
2024-12-18 11:19:55 -0800

    Added a queue on the interceptor, to manage one refresh call at a time.

--- frontend/src/SmmApi.jsx
+++ frontend/src/SmmApi.jsx
@@ -10,24 +10,21 @@ const getConfig = () => {
   };
 };
 
+let refreshingPromise = null;
+
 //To manage refresh token in case the access token is expired
 axios.interceptors.response.use(
   (response) => {
     return Promise.resolve(response);
   },
   function (error) {
-    console.log(error);
-    const { response, message } = error;
+    const { response} = error;
     const detail =
       response.data?.detail ||
       response.data?.error ||
       "Error details not defined";
     const code = response.data?.code || "Code not defined";
-    console.error(message);
-    console.error(`Interceptor error: ${detail}: ${code}`);
-
     const originalRequest = error.config;
-
     if (
       response.status === 401 &&
       !originalRequest._retry &&
@@ -39,28 +36,28 @@ axios.interceptors.response.use(
         detail === "Given token not valid for any token type"
       ) {
         originalRequest._retry = true;
-        const { refresh } = JSON.parse(sessionStorage.getItem("token"));
-        return axios
-          .post(`${BASE_URL}/login/refresh/`, { refresh: refresh })
-          .then((res) => {
-            if (res.status === 200) {
-              console.log(res);
-              let { access, refresh } = res.data;
-              let token = { access, refresh };
-              sessionStorage.setItem("token", JSON.stringify(token));
-              console.log("Access token has been refreshed");
-              originalRequest.headers = getConfig();
-              return axios(originalRequest);
-            }
-          });
+        if (!refreshingPromise) {
+          const { refresh } = JSON.parse(sessionStorage.getItem("token"));
+          refreshingPromise = axios
+            .post(`${BASE_URL}/login/refresh/`, { refresh: refresh })
+            .then((res) => {
+              if (res.status === 200) {
+                let { access, refresh } = res.data;
+                let token = { access, refresh };
+                sessionStorage.setItem("token", JSON.stringify(token));
+              }
+            refreshingPromise = null;
+            });
+        }
+        return refreshingPromise.then(() => {
+          originalRequest.headers = getConfig();
+          return axios(originalRequest);
+        });
       }
       // Refresh Token expired. clean up the tokens stored in sessionStorage
       if (detail === "Token is invalid or expired") {
-        console.log(
-          "Refresh token expired, cleanup tokens and navigate to Login page"
-        );
         sessionStorage.removeItem("token");
-        return (window.location.href = `${BASE_URL}/login`);
+        return (window.location.href = ``);
       }
     }
     return Promise.reject(error);
@@ -239,7 +236,6 @@ export class SmmApi {
       // Cleanup
       link.parentNode.removeChild(link);
     } catch (error) {
-      console.error("Error downloading heats file:", error);
       throw error;
     }
   }
@@ -272,7 +268,6 @@ export class SmmApi {
       // Cleanup
       link.parentNode.removeChild(link);
     } catch (error) {
-      console.error("Error downloading heats file:", error);
       throw error;
     }
   }
@@ -298,7 +293,7 @@ export class SmmApi {
     });
     return res.data;
   }
-  
+
   static async downloadResultsForEvent(swimMeetName, eventName, eventId, data) {
     try {
       const response = await axios.post(
@@ -327,7 +322,6 @@ export class SmmApi {
       // Cleanup
       link.parentNode.removeChild(link);
     } catch (error) {
-      console.error("Error downloading results file:", error);
       throw error;
     }
   }
@@ -360,7 +354,6 @@ export class SmmApi {
       // Cleanup
       link.parentNode.removeChild(link);
     } catch (error) {
-      console.error("Error downloading results file:", error);
       throw error;
     }
   }
