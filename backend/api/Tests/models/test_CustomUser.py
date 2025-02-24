317de94107529e954d6b9ebf122007068c42710d
Sarah <blulady@gmail.com>
2025-02-24 15:39:09 -0800

    tests for models

--- backend/api/Tests/models/test_CustomUser.py
+++ backend/api/Tests/models/test_CustomUser.py
@@ -1,16 +1,12 @@
-"""delete this file not needed"""
-
 from api.models import CustomUser
-from django.core.exceptions import ValidationError
-from django.test import TransactionTestCase, TestCase
+from django.test import TestCase
 
-class CustomUserTestCase(TransactionTestCase):
-    reset_sequences = True
+class CustomUserTestCase(TestCase):
     fixtures = ['setup_data.json']
     _coach = None
 
     def setUp(self):
         self._coach = CustomUser.objects.get(email='coach@email.com')
 
-    def test_name_max_length(self):
-        test_name = 'a'
+    def test_returns_email(self):
+        self.assertEqual(str(self._coach), 'coach@email.com')
