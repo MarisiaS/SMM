8a734d3f94fd2e57cdae7d1eaa45ad19469b7ed3
Elzbieta Sanjuan Szklarz <marisiasanjuan@gmail.com>
2025-05-03 14:42:06 -0700

    Eliminated age calculations from AthleteView.

--- backend/api/views/AthleteView.py
+++ backend/api/views/AthleteView.py
@@ -4,8 +4,6 @@ from rest_framework.permissions import IsAuthenticated
 from django.db.models.functions import Collate
 from api.models import Athlete
 from api.serializers.AthleteSerializer import AthleteSerializer
-from django.db.models import F, Func, IntegerField, Value
-from django.utils import timezone
 from api.CustomFilter import filter_by_group
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 import logging
@@ -20,24 +18,19 @@ class AthleteViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     filter_backends = [OrderingFilter, SearchFilter]
     http_method_names = ['get', 'post', 'delete', 'patch']
-    search_fields = ['^first_name_search','^last_name_search']
+    search_fields = ['^first_name_search', '^last_name_search']
     ordering = ['first_name']
-    
+
     def get_queryset(self):
         queryset = Athlete.objects.annotate(
-        first_name_search=Collate("first_name", "und-x-icu"),
-        last_name_search=Collate("last_name", "und-x-icu"),
-        age=Func(
-                Value("year"),
-                Func(Value(timezone.now().date()), F("date_of_birth"), function="age"),
-                function="date_part",
-                output_field=IntegerField()),
+            first_name_search=Collate("first_name", "und-x-icu"),
+            last_name_search=Collate("last_name", "und-x-icu"),
         )
         group_id = self.request.query_params.get('group_id')
         if group_id is not None:
             queryset = filter_by_group(group_id=group_id, queryset=queryset)
         return queryset
-    
-    @extend_schema(parameters =[OpenApiParameter(name="group_id", type=int),])
+
+    @extend_schema(parameters=[OpenApiParameter(name="group_id", type=int),])
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
