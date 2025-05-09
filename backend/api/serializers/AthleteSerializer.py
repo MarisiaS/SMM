d27716b583adf4e7a5710eb9b2bcf643de52fa1c
Elzbieta María Sanjuan Szklarz <95152310+MarisiaS@users.noreply.github.com>
2025-05-09 10:29:59 -0700

    BE-AthleteSerializer modification (#262)
    * Eliminated age calculations from AthleteView.
    
    * Changed age to be a serializerMethodField and added get_age function to calculate age.
    
    * Eliminated validation of date_of_birth because it is required.

--- backend/api/serializers/AthleteSerializer.py
+++ backend/api/serializers/AthleteSerializer.py
@@ -7,7 +7,7 @@ class AthleteSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField(max_length=50)
     last_name = serializers.CharField(max_length=50)
     full_name = serializers.CharField(read_only=True)
-    age = serializers.IntegerField(read_only=True)
+    age = serializers.SerializerMethodField()
     gender_display = serializers.CharField(source='get_gender_display', read_only=True)
 
 
@@ -15,6 +15,14 @@ class AthleteSerializer(serializers.ModelSerializer):
         model = Athlete
         fields = '__all__'
         read_only_fields = ['full_name', 'age','gender_display']
+
+    def get_age(self, obj):
+        """Calculate age from date_of_birth."""
+        today = timezone.now().date()
+        return today.year - obj.date_of_birth.year - (
+            (today.month, today.day) < (obj.date_of_birth.month, obj.date_of_birth.day)
+        )
+
 
     # Validate that the date is not in the future
     def validate_date_of_birth(self, value):
